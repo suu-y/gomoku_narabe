9ec10d1d27695ca29a2d5811e7e4eaf34a99f315
cgug1090@mail4.doshisha.ac.jp <cgug1090@mail4.doshisha.ac.jp>
2023-11-30 09:30:21 +0900

    長連が起こるケースが「4連-1連」のみだったソースに「2連-3連」の場合も追加

--- kinzite.c
+++ kinzite.c
@@ -15,11 +15,10 @@ int judge_chouren(int x, int y, int board[BOARD_SQUARE][BOARD_SQUARE]){
     int chouren_flag = 0;
     int judge_x_o = board[x][y];    // 今回判定する石の種類
 
-    // ------------- 判定 -------------------------------------
-
+    // =============== 垂直方向 判定 ============================================
     int vertical_upper_exists = 0;        // 垂直方向上向きの長連を探す
-    if(board[x][y-4] == judge_x_o && (y-4) > 0){
-        for(int i = 3; i > 0; i--){
+    if(board[x][y-4] == judge_x_o && (y-4) >= 0){    // 「4連-1連」の長連を判定
+        for(int i=3; i>0; i--){
             if(board[x][y-i] == judge_x_o){
                 vertical_upper_exists = 1;
             }else{
@@ -27,7 +26,24 @@ int judge_chouren(int x, int y, int board[BOARD_SQUARE][BOARD_SQUARE]){
                 break;
             }
         }
-        if((board[x][y+1] == judge_x_o && (y+1) < BOARD_SQUARE + 1)
+        if((board[x][y+1] == judge_x_o && (y+1) < BOARD_SQUARE)
+                && (vertical_upper_exists == 1)){
+            chouren_flag = 1;
+            printf("長連です, ゲーム終了(上)");
+            return 1;
+        }
+    }
+    else if(board[x][y-3] == judge_x_o && (y-3) >= 0){   // 「2連-3連」の長連を判定
+        for(int i=2; i>0; i--){
+            if(board[x][y-i] == judge_x_o){
+                vertical_upper_exists = 1;
+            }else{
+                vertical_upper_exists = 0;
+                break;
+            }
+        }
+        if((board[x][y+1] == judge_x_o && (board[x][y+2] == judge_x_o))
+                && (y+2) < BOARD_SQUARE
                 && (vertical_upper_exists == 1)){
             chouren_flag = 1;
             printf("長連です, ゲーム終了(上)");
@@ -36,7 +52,7 @@ int judge_chouren(int x, int y, int board[BOARD_SQUARE][BOARD_SQUARE]){
     }
 
     int vertical_lower_exists = 0;        // 垂直方向下向きの長連を探す
-    if(board[x][y+4] == judge_x_o && (y+4) < BOARD_SQUARE){
+    if(board[x][y+4] == judge_x_o && (y+4) < BOARD_SQUARE){     // 「4連-1連」の長連を判定
         for(int i = 3; i > 0; i--){
             if(board[x][y+i] == judge_x_o){
                 vertical_lower_exists = 1;
@@ -45,10 +61,98 @@ int judge_chouren(int x, int y, int board[BOARD_SQUARE][BOARD_SQUARE]){
                 break;
             }
         }
-        if((board[x][y-1] == judge_x_o && (y-1) > 0)
+        if((board[x][y-1] == judge_x_o && (y-1) >= 0)
                 && (vertical_lower_exists == 1)){
             chouren_flag = 1;
             printf("長連です, ゲーム終了(下)");
+            return 1;
+        }
+    }
+    else if(board[x][y+3] == judge_x_o && (y+3) < BOARD_SQUARE){   // 「2連-3連」の長連を判定
+        for(int i=2; i>0; i--){
+            if(board[x][y+i] == judge_x_o){
+                vertical_lower_exists = 1;
+            }else{
+                vertical_lower_exists = 0;
+                break;
+            }
+        }
+        if((board[x][y-1] == judge_x_o && (board[x][y-2] == judge_x_o))
+                && (y-2) >= 0
+                && (vertical_lower_exists == 1)){
+            chouren_flag = 1;
+            printf("長連です, ゲーム終了(下)");
+            return 1;
+        }
+    }
+
+    // =============== 水平方向 判定 ============================================
+    int horizontal_right_exists = 0;        // 水平方向右向きの長連を探す
+    if(board[x+4][y] == judge_x_o && (x+4) < BOARD_SQUARE){    // 「4連-1連」の長連を判定
+        for(int i=3; i>0; i--){
+            if(board[x+i][y] == judge_x_o){
+                horizontal_right_exists = 1;
+            }else{
+                horizontal_right_exists = 0;
+                break;
+            }
+        }
+        if((board[x-1][y] == judge_x_o && (x-1) >= 0)
+                && (horizontal_right_exists == 1)){
+            chouren_flag = 1;
+            printf("長連です, ゲーム終了(右)");
+            return 1;
+        }
+    }
+    else if(board[x+3][y] == judge_x_o && (x+3) < BOARD_SQUARE){   // 「2連-3連」の長連を判定
+        for(int i=2; i>0; i--){
+            if(board[x+i][y] == judge_x_o){
+                horizontal_right_exists = 1;
+            }else{
+                horizontal_right_exists = 0;
+                break;
+            }
+        }
+        if((board[x-1][y] == judge_x_o && (board[x-2][y] == judge_x_o))
+                && (x-2) >= 0
+                && (horizontal_right_exists == 1)){
+            chouren_flag = 1;
+            printf("長連です, ゲーム終了(右)");
+            return 1;
+        }
+    }
+
+    int horizontal_left_exists = 0;        // 水平方向左向きの長連を探す
+    if(board[x-4][y] == judge_x_o && (x-4) >= 0){     // 「4連-1連」の長連を判定
+        for(int i = 3; i > 0; i--){
+            if(board[x-i][y] == judge_x_o){
+                horizontal_left_exists = 1;
+            }else{
+                horizontal_left_exists = 0;
+                break;
+            }
+        }
+        if((board[x+1][y] == judge_x_o && (x+1) < BOARD_SQUARE)
+                && (horizontal_left_exists == 1)){
+            chouren_flag = 1;
+            printf("長連です, ゲーム終了(左)");
+            return 1;
+        }
+    }
+    else if(board[x-3][y] == judge_x_o && (x-3) >= 0){   // 「2連-3連」の長連を判定
+        for(int i=2; i>0; i--){
+            if(board[x-i][y] == judge_x_o){
+                horizontal_left_exists = 1;
+            }else{
+                horizontal_left_exists = 0;
+                break;
+            }
+        }
+        if((board[x+1][y] == judge_x_o && (board[x+2][y] == judge_x_o))
+                && (x+2) < BOARD_SQUARE
+                && (horizontal_left_exists == 1)){
+            chouren_flag = 1;
+            printf("長連です, ゲーム終了(左)");
             return 1;
         }
     }
