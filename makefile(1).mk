72234af8be6755f9d292508e6ccbe9c1be04a946
cgug1090@mail4.doshisha.ac.jp <cgug1090@mail4.doshisha.ac.jp>
2023-12-03 23:37:19 +0900

    不要なファイルを整理・削除 ブランチのマージによって複数バージョン存在するファイルを整理した

--- makefile(1).mk
+++
@@ -1,7 +0,0 @@
-# makefile for client_ver1.c and kachimake.c
-client: client_ver1.o kachimake.o
-	gcc -o client client_ver1.o kachimake.o -lws2_32
-client_ver1.o: client_ver1.c
-	gcc -I include -c client_ver1.c -o client_ver1.o -fexec-charset=CP932
-kachimake.o: kachimake.c
-	gcc -I include -c kachimake.c -o kachimake.o -fexec-charset=CP932
