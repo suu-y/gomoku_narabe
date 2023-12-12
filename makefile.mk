8dc14770c7b7a0d8292534c9ee4a506f805828e6
pisumin <harukapsm0722@outlook.jp>
2023-12-13 02:39:49 +0900

    2023-12-13 2:39 テスト用にmakefileを更新

--- makefile.mk
+++ makefile.mk
@@ -1,10 +1,17 @@
-# makefile for client_ver1.c and kachimake.c kinzite.c
+# makefile for client_auto and client_ver1.c and kachimake.c kinzite.c
 # copy this command: mingw32-make -f makefile.mk
-client: client_ver1.o kachimake.o kinzite.o
-	gcc -o client client_ver1.o kachimake.o kinzite.o -lws2_32
+all: client client_auto
+client: client_ver1.o kachimake.o kinzite.o defense.o
+	gcc -o client client_ver1.o kachimake.o kinzite.o defense.o -lws2_32
 client_ver1.o: client_ver1.c
 	gcc -I include -c client_ver1.c -o client_ver1.o -fexec-charset=CP932
+client_auto: client_auto.o kachimake.o kinzite.o defense.o
+	gcc -o client_auto client_auto.o kachimake.o kinzite.o defense.o -lws2_32
+client_auto.o: client_auto.c
+	gcc -I include -c client_auto.c -o client_auto.o -fexec-charset=CP932
 kachimake.o: kachimake.c
 	gcc -I include -c kachimake.c -o kachimake.o -fexec-charset=CP932
 kinzite.o: kinzite.c
 	gcc -I include -c kinzite.c -o kinzite.o -fexec-charset=CP932
+defense.o: defense.c
+	gcc -I include -c defense.c -o defense.o -fexec-charset=CP932
